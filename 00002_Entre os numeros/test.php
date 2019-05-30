bd927c7dee003c7a70e421e3d3ec0751d214f409
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-30 20:36:11 +0000

    Mumuki Export on 2019-05-30 20:36:11 +0000

--- 00002_Entre os numeros/test.php
+++ 00002_Entre os numeros/test.php
@@ -6,14 +6,14 @@ public function testResultado(): void {
   
   $this->assertTrue($fct == 1, "A função deveria receber um parametro.");
   
-  $this->assertTrue(entre100(50) == true, "Dado numero 50 a função retorna true");
-  $this->assertTrue(entre100(0) == true, "La función falla para el número 0");
-  $this->assertTrue(entre100(100) == true, "La función falla para el número 100");
+  $this->assertTrue(entre100(50) == true, "Dado numero 50 a função retorna false");
+  $this->assertTrue(entre100(0) == true, "Dado numero 0 a função retorna false");
+  $this->assertTrue(entre100(100) == true, "Dado numero 100 a função retorna false");
   
-  $this->assertTrue(entre100(200) == false, "La función falla para el número 200");
-  $this->assertTrue(entre100(101) == false, "La función falla para el número 100");
+  $this->assertTrue(entre100(200) == false, "Dado numero 200 a função retorna true");
+  $this->assertTrue(entre100(101) == false, "Dado numero 101 a função retorna true");
   
   
-  $this->assertTrue(entre100(-90) == false, "La función falla para el número -90");
-  $this->assertTrue(entre100(-1) == false, "La función falla para el número -1");
+  $this->assertTrue(entre100(-90) == false, "Dado numero -90 a função retorna true");
+  $this->assertTrue(entre100(-1) == false, "Dado numero -1 a função retorna true");
 }
