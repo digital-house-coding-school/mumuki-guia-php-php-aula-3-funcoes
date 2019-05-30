15f8bfbdbaa66cbd4b47a3637812026c4a89a9db
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-30 22:22:16 +0000

    Mumuki Export on 2019-05-30 22:22:16 +0000

--- 00006_Opa cs/description.md
+++ 00006_Opa cs/description.md
@@ -1,1 +1,4 @@
- d
+
+Seu objetivo é escrever uma ** função ** chamada `umADez` que retorna uma array com números de 1 a 10.
+
+Lembre-se que você pode usar um loop para te ajudar nessa tarefa!
--- 00006_Opa cs/test.php
+++ 00006_Opa cs/test.php
@@ -1,3 +1,18 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
+public function testResultado(): void {
+  $this->assertTrue(function_exists("umADez"), "A função umADez não existe");
+  
+  $resul = umADez();
+  
+  $this->assertTrue(is_array($resul), "A função deve retornar um array");
+  
+  $this->assertTrue(count($resul) == 10, "A array que será retornada deve conter 10 elementos");
+  
+  $exito = true;
+  $i = 1;
+  foreach ($resul as $r) {
+    $exito = $exito && $r == $i;
+    $i++;
+  }
+  
+  $this->assertTrue($exito, "A função não está retornando a contagem correta!");
 }
