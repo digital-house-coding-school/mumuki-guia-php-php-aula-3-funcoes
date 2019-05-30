9826720aab78d6a8fef1ccb4a88bf9c9becc5002
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-05-30 22:02:39 +0000

    Mumuki Export on 2019-05-30 22:02:39 +0000

---
+++ 00005_Variaveis globais/description.md
@@ -0,0 +1,5 @@
+Para este exercício, uma variável global `$contadorGlobal` já está definida!
+
+Esta variável começa em 0 e é um inteiro.
+
+Seu trabalho é escrever uma função chamada `someUm` ** sem parâmetros ** que use essa variável global  e some mais 1 em seu valor!
---
+++ 00005_Variaveis globais/test.php
@@ -0,0 +1,13 @@
+public function testResultado(): void {
+  global $contadorGlobal;
+  $this->assertTrue($contadorGlobal == 0, 'Verifique se você está utilizando a váraivel global $contatorGlobal');
+  
+  $this->assertTrue(function_exists("someUm"), "Não existe a função someUm");
+  
+  someUm();
+  $this->assertTrue($contadorGlobal == 1, 'Aparentemente o valor da váriavel $contatorGlobal não foi alterado.');
+  
+  someUm();
+  someUm();
+  $this->assertTrue($contadorGlobal == 3, 'Aparentemente o valor da váriavel $contatorGlobal não foi alterado.');
+}
