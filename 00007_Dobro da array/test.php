188bed297facfce75f7baea29e2fdbe6ee357e8a
vinicius@digitalhouse.com <vinicius@digitalhouse.com>
2019-06-01 00:05:26 +0000

    Mumuki Export on 2019-06-01 00:05:26 +0000

--- 00007_Dobro da array/test.php
+++ 00007_Dobro da array/test.php
@@ -1,7 +1,7 @@
 public function testResultado(): void {
   $sol = '/*...content...*/';
   
-  $this->assertTrue(function_exists("dobroDaArray"), "Não existe a função 'dobroDaArray'.");
+  $this->assertTrue(function_exists(''), "Não existe a função 'dobroDaArray'.");
   
   $resul = $this->dobroDaArray();
   
